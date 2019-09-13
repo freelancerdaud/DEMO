fabb718e106c354fd7a922434001ef903518491e
MD ABU DAUD <freelancerdaud@gmail.com>
2019-09-13 21:58:39 +0600

    somthing text added

---
+++ New Project/JS/mine.js
@@ -0,0 +1,38 @@
+document.getElementById("show_button").addEventListener("click",function(){
+
+
+document.getElementById("demo1").setAttribute("style","display:block;opacity:1;transition: 1s;");
+
+
+
+});
+
+document.getElementById("hide_button").addEventListener("click",function(){
+
+
+ document.getElementById("demo1").setAttribute("style","display:none;transition: 1s;");
+
+
+
+});
+
+
+
+document.getElementById("show_button1").addEventListener("click",function(){
+
+
+document.getElementById("demo").setAttribute("style","display:block;opacity:1;transition: 5s;");
+
+
+
+
+});
+
+document.getElementById("hide_button1").addEventListener("click",function(){
+
+
+document.getElementById("demo").setAttribute("style","display:none;transition: 5s;");
+
+
+
+});
