e69cb3b51db6515414f3b0702990e82abd0f0a49
MD ABU DAUD <freelancerdaud@gmail.com>
2019-09-13 22:54:33 +0600

    add

--- New Project/JS/mine.js
+++ New Project/JS/mine.js
@@ -18,21 +18,42 @@ document.getElementById("hide_button").addEventListener("click",function(){
 
 
 
-document.getElementById("show_button1").addEventListener("click",function(){
+document.getElementById("show_button1").addEventListener("mouseover",function(){
 
 
-document.getElementById("demo").setAttribute("style","display:block;opacity:1;transition: 5s;");
+document.getElementById("demo").setAttribute("style","opacity:1;transition: 5s;transform:rotate(360deg)");
 
+
+
+
+});
+
+document.getElementById("hide_button1").addEventListener("mouseover",function(){
+
+
+document.getElementById("demo").setAttribute("style","");
 
 
 
 });
 
-document.getElementById("hide_button1").addEventListener("click",function(){
 
 
-document.getElementById("demo").setAttribute("style","display:none;transition: 5s;");
+document.getElementById("show1").addEventListener("click",clicek1);
+function clicek1(){
+
+document.getElementById("demo2").setAttribute("style","opacity:1;transform:rotate(360deg);");
 
 
 
-});
+
+}
+document.getElementById("hide1").addEventListener("click",clickda2);
+function clickda2(){
+
+document.getElementById("demo2").setAttribute("style","");
+
+
+
+
+}
